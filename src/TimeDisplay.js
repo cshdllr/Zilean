11eadc596556787569a3ad80fa2bb955ccf35bc1
Jason Cashdollar <jason.cashdollar@gmail.com>
2016-10-07 10:30:14 -0700

    use props because react
    switched time variable to props

--- src/TimeDisplay.js
+++ src/TimeDisplay.js
@@ -3,37 +3,55 @@
 import React, { Component } from 'react';
 import {
   TouchableHighlight,
-  Text
+  Text,
+  View
 } from 'react-native';
 import moment from 'moment/src/moment';
 
 import Style from './Style';
 
-export default class TimeDisplay extends Component {
-  render() {
-    return (
-      <TouchableHighlight onPress={this._onTimeTap()}>
-        <Text style={Style.time}>{this.getInitialTime()}</Text>
-      </TouchableHighlight>
-    )
-  }
-
-  getInitialTime() {
-    let h0 = moment().format('h');
-    let m0 = moment().format('mm');
-
-    if (m0 > 15) {
-      let m1 = 55;
-      let h1 = h0;
-      return h1 + ":" + m1;
-    } else {
-      let m1 = 25;
-      let h1 = h0;
-      return h1 + ":" + m1;
-    }
+class Time extends React.Component {
+  static defaultProps = {
+    hour: 12,
+    minute: 12,
   }
 
   _onTimeTap() {
     console.log("lol")
+  }
+
+  render() {
+    return (
+      <TouchableHighlight onPress={this._onTimeTap()}>
+        <Text style={Style.time}>{this.props.hour}:{this.props.minute}</Text>
+      </TouchableHighlight>
+    );
+  }
+}
+
+export default class TimeDisplay extends React.Component {
+
+  getInitialHour() {
+    return moment().format('h');
+  }
+
+  getInitialMinute() {
+    let m0 = moment().format('mm');
+
+    if (m0 > 15) {
+      let m1 = 55;
+      return m1;
+    } else {
+      let m1 = 25;
+      return m1;
+    }
+  }
+
+  render() {
+    return (
+      <View>
+          <Time hour={this.getInitialHour()} minute={this.getInitialMinute()}/>
+      </View>
+    )
   }
 }
